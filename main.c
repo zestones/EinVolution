6cf6ea8799a2ebb32dd08737223067b31970b2b9
Antoine <antoinechataignier1998@orange.fr>
2022-03-28 21:28:37 +0200

    feat: init main.c

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "GL/glut.h"
+#include "GL/gl.h"
+
+#include <math.h>
+
+#include "window.h"
+
+static void Init(void) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, 1.0, 0.1, 1000.0);
+}
+
+static void Key(unsigned char key, int x, int y) {
+    switch (key) {
+        case 27: exit(EXIT_SUCCESS);
+    }
+}
+
+static void Draw(void) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
+
 int main(int argc, char *argv[]) {
-    fprintf(stdout, "Hello world !\n");
+    glutInit(&argc, argv);
+
+    glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+    glutInitWindowPosition(WIN_X, WIN_Y);
+    glutCreateWindow("EinVolution");
+
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glEnable(GL_DEPTH_TEST);
+
+    Init();
+
+    glutDisplayFunc(Draw);
+	glutKeyboardFunc(Key);
+
+    glutMainLoop(); 
+
     return EXIT_SUCCESS;
 }
